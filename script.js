d9a29e034a714396e18544b1c1f3d6c036bd01bd
JaZa <47424360+JaZax@users.noreply.github.com>
2019-08-01 18:15:18 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const canvas = document.querySelector('canvas');
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+window.addEventListener('resize' , ()=>{
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+})
+
+const c = canvas.getContext('2d');
+
+
+var image = new Image();
+    image.src = 'dvd.png';
+    image.height = 200
+
+let x = window.innerWidth/2 - image.width/2;
+let y = window.innerHeight/2 - image.height/2;
+let xspeed = 3;
+let yspeed = 3;
+
+function animation()
+{
+    requestAnimationFrame(animation);
+
+    c.clearRect(0 , 0 , window.innerWidth, window.innerHeight)
+    c.drawImage(image, x, y);
+
+    if(x > window.innerWidth - image.width || x < 0)
+    {
+        xspeed = -xspeed
+    }
+
+    if(y > window.innerHeight - image.height / 2 || y < 0)
+    {   
+        yspeed = -yspeed
+    }
+
+    y += yspeed
+    x += xspeed
+}
+
+animation()
